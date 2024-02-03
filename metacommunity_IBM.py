c195751c5090da7ea976bede94e875214285e6a9
Jianhao Lin <536351843@qq.com>
2024-02-03 09:51:41 +0800

    Update metacommunity_IBM.py

--- metacommunity_IBM.py
+++ metacommunity_IBM.py
@@ -4,7 +4,7 @@ Created on Tue May 17 09:32:30 2022
 
 @title: metacommunuity individuals based model version 2.9.12
 
-@author: JH_Lin
+@author: Unvieling it after the peers review of the article
 """
 
 import numpy as np
